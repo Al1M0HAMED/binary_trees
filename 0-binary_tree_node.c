cf8e2601fe485411e4ccd457d7117962790bbb8b
Ali Mohamed <alimohamed@Alis-MacBook-Air.local>
2026-07-06 23:16:15 +0300

    i'm back agian smile

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 	new_node->n = value;
-	new_node->parent = parent;
+	if (parent)
+		new_node->parent = parent;
 	return (new_node);
 }
